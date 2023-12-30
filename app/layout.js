a9a342c875e19eefba7a7d797165342153ad237c
Bulut Yerli <bulutyerli@gmail.com>
2023-12-30 04:03:56 +0300

    vercel analytics

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Cormorant } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 const cormorant = Cormorant({
   subsets: ["latin"],
@@ -20,6 +21,7 @@ export default function RootLayout({ children }) {
       <body className={cormorant.className}>
         <Header />
         {children}
+        <Analytics />
         <Footer />
       </body>
     </html>
